cdd8a567081fad799075ccefc409a7f2c1940626
Josimar Silva <josimaralves.ti@gmail.com>
2025-08-23 16:21:19 +0200

    feat(blog-43): export blog as Static Website

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,17 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
+    output: 'export',
+    distDir: 'dist/blog',
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+    images: {
+        unoptimized: true,
+    },
 }
 
 export default nextConfig
