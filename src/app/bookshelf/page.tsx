4f578afe39e38e2c465a67d335b88925695be3b3
Josimar Silva <josimaralves.ti@gmail.com>
2025-08-23 16:11:03 +0200

    chore: make bookshelf pages exportable

--- src/app/bookshelf/page.tsx
+++ src/app/bookshelf/page.tsx
@@ -2,9 +2,11 @@ import { BlogHeader } from "@/app/__components/blog-header"
 import { BookshelfHero } from "@/app/__components/bookshelf-hero"
 import { BookshelfList } from "@/app/__components/bookshelf-list"
 import { BlogFooter } from "@/app/__components/blog-footer"
-import { books } from "@/lib/books"
+import { getBooks } from "@/lib/books"
 
-export default function BookshelfPage() {
+export default async function BookshelfPage() {
+  const books = await getBooks()
+
   return (
     <div className="min-h-screen bg-background">
       <BlogHeader />
