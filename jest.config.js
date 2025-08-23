fea69b8dd17c490190bd7aeeaee84b1c90f5adb8
Josimar Silva <josimaralves.ti@gmail.com>
2025-08-23 16:48:58 +0200

    feat(#43): configure jest

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    testEnvironment: 'jsdom',
+    testPathIgnorePatterns: ['/node_modules/', '/.next/', '/ui-tests/'],
+    testMatch: ['<rootDir>/src/(app|hooks|interfaces|lib)/**/*.test.(ts|tsx)'],
+    moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/$1',
+    },
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+    transform: {
+        '^.+\.(ts|tsx)$': ['@swc/jest', {
+            jsc: {
+                transform: {
+                    react: {
+                        runtime: 'automatic'
+                    }
+                }
+            }
+        }],
+    },
+    extensionsToTreatAsEsm: ['.ts', '.tsx'],
+};
