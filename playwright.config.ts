e01bef65c1931771fe1a1d106c03f84656523065
Josimar Silva <josimaralves.ti@gmail.com>
2025-08-27 17:25:16 +0200

    ci(e2e-tests): use github reporter on playwright tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig, devices } from "@playwright/test";
+import type { GitHubActionOptions } from "@estruyf/github-actions-reporter";
 
 /**
  * Read environment variables from file.
@@ -22,7 +23,15 @@ export default defineConfig({
   timeout: 60_000,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
-    ["html", { outputFolder: "test-results", open: "never" }],
+    ["list"],
+    [
+      "@estruyf/github-actions-reporter",
+      <GitHubActionOptions>{
+        title: "E2E Tests",
+        useDetails: true,
+        showError: true,
+      },
+    ],
     [
       "monocart-reporter",
       {
@@ -44,8 +53,7 @@ export default defineConfig({
     baseURL: "http://localhost:3000",
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
-    /* Collect screenshot of the test failures */
-    screenshot: "only-on-failure",
+    screenshot: "off",
   },
 
   /* Configure projects for major browsers */
