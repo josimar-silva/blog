9746a92de91b372e1258829ace9b8f1c24368b10
Josimar Silva <josimaralves.ti@gmail.com>
2025-08-29 10:09:47 +0200

    feat(categories): generate categories manifest during build

--- scripts/prebuild.mjs
+++ scripts/prebuild.mjs
@@ -1,7 +1,8 @@
 import { generateManifest } from "./generate-post-manifest.mjs";
+import { generateCategoriesManifest } from "./generate-categories-manifest.mjs";
 
 console.log("Starting prebuild process...");
 
-generateManifest();
+Promise.all([generateManifest(), generateCategoriesManifest()]);
 
 console.log("Finished prebuild process...");
