f2360a59f35b6270627af2bbd574aabe19fd6b49
Josimar Silva <josimaralves.ti@gmail.com>
2025-09-22 11:36:35 +0200

    build(books): add script to generate books manifest from markdown

--- scripts/prebuild.mjs
+++ scripts/prebuild.mjs
@@ -1,8 +1,9 @@
 import { generateManifest } from "./generate-post-manifest.mjs";
 import { generateCategoriesData } from "./generate-categories-data.mjs";
+import { generateBooksManifest } from "./generate-books-manifest.mjs";
 
 console.log("Starting prebuild process...");
 
-Promise.all([generateManifest(), generateCategoriesData()]);
+Promise.all([generateManifest(), generateCategoriesData(), generateBooksManifest()]);
 
 console.log("Finished prebuild process...");
