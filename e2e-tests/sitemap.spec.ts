e088a82a0d4f8f9865e7f05c468cf9e1c127949d
Josimar Silva <josimaralves.ti@gmail.com>
2025-10-09 10:56:40 +0200

    feat: add sitemap
    Closes #185

---
+++ e2e-tests/sitemap.spec.ts
@@ -0,0 +1,33 @@
+import { test, expect } from "@playwright/test";
+
+test.describe("Sitemap", () => {
+  test("should be accessible and have the correct content type", async ({
+    request,
+  }) => {
+    const response = await request.get("/sitemap.xml");
+    expect(response.status()).toBe(200);
+    expect(response.headers()["content-type"]).toBe("application/xml");
+  });
+
+  test("should contain the main pages", async ({ request }) => {
+    const response = await request.get("/sitemap.xml");
+    const sitemap = await response.text();
+    expect(sitemap).toContain("<loc>https://josimar-silva.com/</loc>");
+    expect(sitemap).toContain("<loc>https://josimar-silva.com/about</loc>");
+    expect(sitemap).toContain("<loc>https://josimar-silva.com/blog</loc>");
+    expect(sitemap).toContain("<loc>https://josimar-silva.com/bookshelf</loc>");
+    expect(sitemap).toContain("<loc>https://josimar-silva.com/contact</loc>");
+    expect(sitemap).toContain("<loc>https://josimar-silva.com/now</loc>");
+    expect(sitemap).toContain("<loc>https://josimar-silva.com/privacy</loc>");
+    expect(sitemap).toContain("<loc>https://josimar-silva.com/projects</loc>");
+    expect(sitemap).toContain("<loc>https://josimar-silva.com/terms</loc>");
+  });
+
+  test("should contain blog posts", async ({ request }) => {
+    const response = await request.get("/sitemap.xml");
+    const sitemap = await response.text();
+    expect(sitemap).toContain(
+      "<loc>https://josimar-silva.com/blog/2025-01-21-rinha-de-backend-2025</loc>",
+    );
+  });
+});
---
+++ src/app/sitemap.ts
@@ -0,0 +1,54 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2025 Josimar Silva
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+import { MetadataRoute } from "next";
+
+import { getAllPosts } from "@/lib/posts";
+
+export const dynamic = "force-static";
+
+const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || "https://josimar-silva.com";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const posts = await getAllPosts(["slug", "date"]);
+
+  const postUrls = posts.map((post) => ({
+    url: `${siteUrl}/blog/${post.slug}`,
+    lastModified: new Date(post.date),
+  }));
+
+  const staticPages = [
+    { url: `${siteUrl}/`, lastModified: new Date() },
+    { url: `${siteUrl}/about`, lastModified: new Date() },
+    { url: `${siteUrl}/blog`, lastModified: new Date() },
+    { url: `${siteUrl}/bookshelf`, lastModified: new Date() },
+    { url: `${siteUrl}/contact`, lastModified: new Date() },
+    { url: `${siteUrl}/now`, lastModified: new Date() },
+    { url: `${siteUrl}/privacy`, lastModified: new Date() },
+    { url: `${siteUrl}/projects`, lastModified: new Date() },
+    { url: `${siteUrl}/terms`, lastModified: new Date() },
+  ];
+
+  return [...staticPages, ...postUrls];
+}
